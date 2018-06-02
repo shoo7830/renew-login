d076ae03c88ea7fabbbefa462d658146e6d57b73
KimIn Suk <shoo7830@gmail.com>
2018-06-02 23:52:56 +0900

    로그인 폼 컴포넌트 영역 분

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,19 @@
 <template>
   <div id="app">
-    <form>
-        <section>
-            <div>
-                <!-- SVG 영역 -->
-            </div>
-            <div class="input-group">
-                <input type="text" placeholder="아이디">
-            </div>
-            <div class="input-group">
-                <input type="password" placeholder="비밀번호">
-            </div>
-            <div class="input-group">
-                <button class="submit-button">로그인</button>
-            </div>
-            
-        </section>
-    </form>
+    <login-form></login-form>
   </div>
 </template>
 
 <script>
+import LoginForm from './components/LoginForm.vue';
 export default {
   name: 'app',
+  components:{
+    LoginForm,
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      
     }
   }
 }
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <form>
+        <section>
+            <div>
+                <!-- SVG 영역 -->
+            </div>
+            <div class="input-group">
+                <input type="text" placeholder="아이디">
+            </div>
+            <div class="input-group">
+                <input type="password" placeholder="비밀번호">
+            </div>
+            <div class="input-group">
+                <button class="submit-button">로그인</button>
+            </div>
+            
+        </section>
+    </form>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+
+</style>
